44e418471664b714264a6fb8fb68de5d7ea10854
epsilon11101 <amirandav1991@gmail.com>
2023-06-27 17:03:09 -0600

    feat: characters component

---
+++ src/components/characters.js
@@ -0,0 +1,78 @@
+import "../css/characters.css";
+import { fetchResource, createLoadButtonEventListener } from "./common";
+import { getCharacters } from "../services/swapi";
+import { progressTemplate } from "./UI";
+
+localStorage.setItem("contador", "1");
+const main = document.querySelector("main");
+main.insertAdjacentHTML("afterbegin", progressTemplate());
+const charactersContainer = document.querySelector("#characters");
+const loadMoreBtn = document.querySelector("#load_more");
+const loadLessBtn = document.querySelector("#load_less");
+
+const spinner = document.querySelector(".spinner");
+
+const characterTemplate = (character, id) => {
+  const {
+    name,
+    height,
+    mass,
+    hair_color,
+    skin_color,
+    eye_color,
+    birth_year,
+    gender,
+  } = character;
+
+  return `
+  <div class="uniqueCharacter ">
+        <p class="pcollapse">
+            <a class="btn " role="button">${name}</a>
+        </p>
+        <div class="col">
+            <div class="" id="char${id}">
+                <div class="card card-body">
+                    <div class="card-char">
+                        <h6>Characteristics</h6>
+                        <div>
+                            <p>Height: ${height}</p>
+                            <p>Mass: ${mass}</p>
+                            <p>Hair color: ${hair_color}</p>
+                            <p>Skin color: ${skin_color}</p>
+                            <p>Eye color: ${eye_color}</p>
+                            <p>Birth Year: ${birth_year}</p>
+                            <p>Gender: ${gender}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+  <div>
+    `;
+};
+
+createLoadButtonEventListener(
+  loadMoreBtn,
+  getCharacters,
+  charactersContainer,
+  characterTemplate,
+  spinner,
+  main
+);
+createLoadButtonEventListener(
+  loadLessBtn,
+  getCharacters,
+  charactersContainer,
+  characterTemplate,
+  spinner,
+  main
+);
+
+fetchResource(
+  1,
+  getCharacters,
+  charactersContainer,
+  characterTemplate,
+  spinner,
+  main
+);
