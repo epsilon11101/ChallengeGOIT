0c77a77760b6bbc7a102fedcda918ed2325a82e4
epsilon11101 <amirandav1991@gmail.com>
2023-06-27 17:31:48 -0600

    feat: add footer template

--- src/components/UI.js
+++ src/components/UI.js
@@ -29,3 +29,20 @@ export const hideProgress = (spinner) => {
   spinner.classList.remove("show");
   spinner.classList.add("hide");
 };
+
+export const footer = () => {
+  return `
+    <footer>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+              <p class="text-center">
+                <a href="https://github.com/epsilon11101" target="_blank">amv</a> - 2023 - <span>made
+                  the force be with you<span />
+                </p>
+              </div>
+            </div>
+          </div>
+    </footer>
+    `;
+};
