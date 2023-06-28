e997792591ee3daa93a4413124b94dbb8eca15a2
epsilon11101 <amirandav1991@gmail.com>
2023-06-27 23:19:31 -0600

    feat: add assets to get image

---
+++ src/components/charactersAssets.js
@@ -0,0 +1,103 @@
+import r5d4 from "../assets/characters/r5d4.jpg";
+import r2d2 from "../assets/characters/r2d2.jpg";
+import owenlars from "../assets/characters/owenlars.jpg";
+import obiwan from "../assets/characters/obiwankenobi.jpg";
+import lukeskywalker from "../assets/characters/lukeskywalker.jpg";
+import leiaorgana from "../assets/characters/leiaorgana.png";
+import darthvader from "../assets/characters/darthvader.jpg";
+import c3po from "../assets/characters/c3po.jpg";
+import biggsdarklighter from "../assets/characters/biggsdarklighter.jpg";
+import beruwhitesunlars from "../assets/characters/beruwhitesunlars.jpg";
+import anakinskywalker from "../assets/characters/anakinskywalker.jpg";
+import wilhufftarkin from "../assets/characters/wilhufftarkin.png";
+import chewbacca from "../assets/characters/chewbacca.jpg";
+import hansolo from "../assets/characters/hansolo.jpg";
+import greedo from "../assets/characters/greedo.jpg";
+import jabbadesilijictiure from "../assets/characters/jabbadesilijictiure.jpg";
+import wedgesantilles from "../assets/characters/wedgeantilles.jpg";
+import jektonoporkins from "../assets/characters/jektonoporkins.jpg";
+import yoda from "../assets/characters/yoda.jpg";
+import palpatine from "../assets/characters/palpatine.jpg";
+
+export const charactersAssets = [
+  {
+    name: "R5-D4",
+    image: r5d4,
+  },
+  {
+    name: "R2-D2",
+    image: r2d2,
+  },
+  {
+    name: "Owen Lars",
+    image: owenlars,
+  },
+  {
+    name: "Obi-Wan Kenobi",
+    image: obiwan,
+  },
+  {
+    name: "Luke Skywalker",
+    image: lukeskywalker,
+  },
+  {
+    name: "Leia Organa",
+    image: leiaorgana,
+  },
+  {
+    name: "Darth Vader",
+    image: darthvader,
+  },
+  {
+    name: "C-3PO",
+    image: c3po,
+  },
+  {
+    name: "Biggs Darklighter",
+    image: biggsdarklighter,
+  },
+  {
+    name: "Beru Whitesun lars",
+    image: beruwhitesunlars,
+  },
+  {
+    name: "Anakin Skywalker",
+    image: anakinskywalker,
+  },
+  {
+    name: "Wilhuff Tarkin",
+    image: wilhufftarkin,
+  },
+  {
+    name: "Chewbacca",
+    image: chewbacca,
+  },
+  {
+    name: "Han Solo",
+    image: hansolo,
+  },
+  {
+    name: "Greedo",
+    image: greedo,
+  },
+  {
+    name: "Jabba Desilijic Tiure",
+    image: jabbadesilijictiure,
+  },
+  {
+    name: "Wedge Antilles",
+    image: wedgesantilles,
+  },
+  {
+    name: "Jek Tono Porkins",
+    image: jektonoporkins,
+  },
+  {
+    name: "Yoda",
+    image: yoda,
+  },
+  {
+    name: "Palpatine",
+    image: palpatine,
+  },
+];
